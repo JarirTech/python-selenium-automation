656f5eb8173f5584c60f5c37385a449b09547591
JarirTech <bjarir001@gmail.com>
2025-05-22 20:01:34 -0400

    update sample_script file

--- sample_script.py
+++ sample_script.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
+
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -18,7 +20,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('table')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +29,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'table'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
